0fb384ef9a13ea7b75e65b4ffed6123fa600734a
Tanuj Soni <mail@tanuj.in>
2024-04-04 13:27:01 +0530

    Fix smoke test

--- src/providers/ethers.provider.spec.ts
+++ src/providers/ethers.provider.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EthersProvider } from './ethers.provider';
+import { ConfigService } from '@nestjs/config';
 
 describe('EthersProvider', () => {
   let provider: EthersProvider;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [EthersProvider],
+      providers: [EthersProvider, ConfigService],
     }).compile();
 
     provider = module.get<EthersProvider>(EthersProvider);
